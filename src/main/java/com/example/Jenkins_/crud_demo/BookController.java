ccccfa4148376ce7e9a805aa80a1a9828cecb27d
sunitsai <58470631+sunitsai@users.noreply.github.com>
2025-06-19 09:30:28 +0530

    Update BookController.java

--- src/main/java/com/example/Jenkins_/crud_demo/BookController.java
+++ src/main/java/com/example/Jenkins_/crud_demo/BookController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public class BookController {
     private final BookRepo bookRepository;
 
-    public BookController(BookRepository bookRepository) {
+    public BookController(BookRepo bookRepository) {
         this.bookRepository = bookRepository;
     }
 
